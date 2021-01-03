313760b8a908336fdbb7aa5c30e7778afc5a6b10
佐々木遥輝 <sasakiharuki@sasakiharukinoMacBook-Pro.local>
2021-01-03 23:44:02 +0900

    モジュールの練習
    しまぶーのjavascript講座youtube参照

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// CORSエラーが発生したため試せてはいない...
+import { name, name2 } from "./user.js";
+
+document.body.textContent = name + name2;
---
+++ js/user.js
@@ -0,0 +1,5 @@
+// CORSエラーが発生したため試せてはいない...
+const name = "ピカチュウ";
+const name2 = "ライチュウ";
+
+export { name, name2 };
